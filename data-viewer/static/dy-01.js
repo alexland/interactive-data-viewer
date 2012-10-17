60d698d6bef279e60b2afb72a125ec40109ee613
doug ybarbo <doug.ybarbo@mac.com>
2012-10-17 03:46:45 -0700

    added input for a data file + code to handle attribute names

--- data-viewer/static/dy-01.js
+++ data-viewer/static/dy-01.js
@@ -20,40 +20,50 @@ $(function() {
 			.tickSize(7)
 			.orient("left")
 			.tickSubdivide(true);
-		init();
 
 	function init() {
-		vis.append("svg:g")
-			.attr("class", "x axis")
-			.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
-			.call(xAxis);
-		vis.append("svg:g")
-			.attr("class", "y axis")
-			.attr("transform", "translate(" + (MARGINS.left) + ",0)")
-			.call(yAxis);
+		d3.csv("data-test1.csv", function(data) {
+			vis.append("svg:g")
+				.attr("class", "x axis")
+				.attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+				.call(xAxis);
+			vis.append("svg:g")
+				.attr("class", "y axis")
+				.attr("transform", "translate(" + (MARGINS.left) + ",0)")
+				.call(yAxis);
+			update(data);
+		});
 	}
 
-	function update() {
-		var newData = randData(),
-			circles = vis.selectAll("circle")
-				.data(newData, function (d) {
-					return d.id;
+	function redraw() {
+
+	});
+
+	function update(drawingData) {
+		var circles = vis.selectAll("circle")
+				.data(drawingData, function (d) {
+					return d.name;
 				}),
 			// axes aren't data points,
 			// so transition object req to change their values
-			transition = vis
-				.transition()
-				.duration(750)
-				.ease("linear");
+		transition = vis.transition().duration(750).ease("linear");
 		// update the domain of the x range
 		xRange.domain([
-			d3.min (newData, function(d) { return d.value1; }),
-			d3.max (newData, function(d) { return d.value1; })
+			d3.min (drawingData, function(d) {
+				return +d['inversions'];
+			}),
+			d3.max (drawingData, function(d) {
+				return +d['inversions'];
+			})
 		]);
 		// update the domain of the y range
 		yRange.domain([
-			d3.min (newData, function(d) { return d.value2; }),
-			d3.max (newData, function(d) { return d.value2; })
+			d3.min (drawingData, function(d) {
+				return +d['speed'];
+			}),
+			d3.max (drawingData, function(d) {
+				return +d['speed'];
+			})
 		]);
 		// transition the axes
 		transition.select(".x.axis").call(xAxis);
@@ -61,40 +71,33 @@ $(function() {
 		circles
 			.enter()
 			.insert("svg:circle")
-			.attr("cx", function (d) { return xRange (d.value1); })
-			.attr("cy", function (d) { return yRange (d.value2); })
+			.attr("cx", function (d) {
+				return xRange (+d['inversions']);
+			})
+			.attr("cy", function (d) {
+				return yRange (+d['speed']);
+			})
 			.style("fill", "red");
 		circles
 			.transition()
 			.duration(750)
 			.ease("linear")
-			.attr("cx", function (d) { return xRange(d.value1); })
-			.attr("cy", function (d) { return yRange(d.value2); })
-			.attr("r", function (d) { return d.value3; });
+			.attr("cx", function (d) {
+				return xRange(+d['inversions']);
+			})
+			.attr("cy", function (d) {
+				return yRange(+d['speed']);
+			})
+			.attr("r", function (d) {
+				return (+d.opened - 1989)/5;
+			});
 		circles.exit ()
 			.transition().duration(750).ease("linear")
 			.attr("r", 0)
 			.remove ();
 	}
 
-	function randData() {
-		var n = randRange(2, 6),
-			dataset = [];
-		for (var i=0; i<n; i++) {
-			var data = {
-				id: i,
-				value1: randRange (1, 10),
-				value2: randRange (0, 100),
-				value3: randRange (2, 10)
-			};
-			dataset.push (data);
-		}
-		return dataset;
-	}
-
-	function randRange(min, max) {
-		return Math.round (Math.random() * (max - min) + min);
-	}
+	init();
 
 	$("#btn1").on("click", update);
 
